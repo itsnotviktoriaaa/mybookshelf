4eb4e66a085a581d90d9fe3a771ff45af114ca34
Victoria Goltseva <katnisseverdeen191@mail.ru>
2024-03-21 12:52:27 +0300

    added config files for diff linters

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './views/user/login/login.component';
+import { SignupComponent } from './views/user/signup/signup.component';
+
+@NgModule({
+  declarations: [LoginComponent, SignupComponent],
+  imports: [],
+  providers: [],
+})
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/user/login/login.component';
+import { SignupComponent } from './views/user/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: 'login' },
+];
