c01cc4fbd0cf1c413dfb7f8dc2faaf096d43c7a4
Victoria Goltseva <katnisseverdeen191@mail.ru>
2024-03-28 20:05:06 +0300

    feat: open and close mini-modals in header

--- src/app/shared/components/layout/header/header.component.ts
+++ src/app/shared/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { HeaderClickInterface } from '../../../../../types/header.interface';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  allMiniModal: boolean = false;
+  langMiniModal: boolean = false;
+  profileMiniModal: boolean = false;
+  protected readonly HeaderClickInterfaceEnum = HeaderClickInterface;
+
+  constructor() {}
+
+  openOrCloseMiniModal(nameOfMiniModal: HeaderClickInterface.allMiniModal | HeaderClickInterface.langMiniModal | HeaderClickInterface.profileMiniModal): void {
+    if (nameOfMiniModal === HeaderClickInterface.allMiniModal) {
+      this.allMiniModal = !this.allMiniModal;
+    }
+    if (nameOfMiniModal === HeaderClickInterface.langMiniModal) {
+      this.langMiniModal = !this.langMiniModal;
+    }
+    if (nameOfMiniModal === HeaderClickInterface.profileMiniModal) {
+      this.profileMiniModal = !this.profileMiniModal;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  click(event: Event) {
+    console.log(event.target);
+    if (this.allMiniModal && !((event.target as Element).closest('.header-search-modal') || (event.target as Element).closest('.header-search-all'))) {
+      this.allMiniModal = false;
+    }
+
+    if (this.langMiniModal && !((event.target as Element).closest('.header-lang-modal') || (event.target as Element).closest('.header-lang-info'))) {
+      this.langMiniModal = false;
+    }
+
+    if (this.profileMiniModal && !((event.target as Element).closest('.header-account-modal') || (event.target as Element).closest('.header-account-info'))) {
+      this.profileMiniModal = false;
+    }
+  }
+}
---
+++ src/types/header.interface.ts
@@ -0,0 +1,5 @@
+export enum HeaderClickInterface {
+  allMiniModal,
+  langMiniModal,
+  profileMiniModal,
+}
