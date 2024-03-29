d68e0aa5a5a4717b02fd87db7e2d343d8dcb5499
Victoria Goltseva <katnisseverdeen191@mail.ru>
2024-03-29 22:07:25 +0300

    uddate package and package.lock json

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-let express = require('express');
+const express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname + '/dist/frontend/'));
+app.use(express.static(__dirname + '/dist/frontend/browser'));
 
 app.get('/*', (req, resp) => {
-  resp.sendFile(__dirname + '/dist/frontend/index.html');
+  resp.sendFile(__dirname + '/dist/frontend/browser/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
