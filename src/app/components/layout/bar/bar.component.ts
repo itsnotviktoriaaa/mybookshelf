d8e9082e872898cc163523b0648b47edfc2ae465
Victoria Goltseva <katnisseverdeen191@mail.ru>
2024-05-04 15:00:50 +0300

    feat: adaptive for bar and header before 955px

--- src/app/components/layout/bar/bar.component.ts
+++ src/app/components/layout/bar/bar.component.ts
@@ -3,12 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IMenuBelowBar, IMenuItem } from '../../../modals/user';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SvgIconComponent } from 'angular-svg-icon';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-bar',
   standalone: true,
-  imports: [NgOptimizedImage, RouterLink, RouterLinkActive, SvgIconComponent],
+  imports: [RouterLink, RouterLinkActive, SvgIconComponent],
   templateUrl: './bar.component.html',
   styleUrl: './bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
